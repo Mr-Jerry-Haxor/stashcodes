97e81e27fddba253c708d8b65c57746379c4b23b
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-05-22 17:50:22 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.home, name='home'),
     path('apply/', views.apply, name='apply'),
 ]
