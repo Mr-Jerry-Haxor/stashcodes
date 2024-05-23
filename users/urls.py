22ce5a6aaf29859954a83ba5f4352ff883dc3939
SHRAVAN JERIPOTHULA <98485154+Mr-Jerry-Haxor@users.noreply.github.com>
2024-05-23 17:22:30 +0530

    s

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('apply/', views.apply, name='apply'),
-    path('contact/', views.contact_us, name='contact_us'),
+    path('contact/', views.contact_us, name='contact'),
 ]
