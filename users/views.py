4580635e425c5a58a3ce3bfcfb0cc65f4296b42d
SHRAVAN JERIPOTHULA <shravanjerry2001@gmail.com>
2024-10-30 19:03:14 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -51,7 +51,7 @@ def apply(request):
 @method_decorator(csrf_exempt, name='dispatch')
 class PaymentWebhook(APIView):
     def post(self, request, *args, **kwargs):
-        data = json.loads(request.body)
+        data = json.loads(request.data)
         
         # Store data in webhook_logs
         log_serializer = WebhookLogsSerializer(data={'log': json.dumps(data)})
